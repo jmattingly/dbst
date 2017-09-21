68b3d6f89bc5db52b377522d798c208f020e7427
jmattingly <john.mattingly19@gmail.com>
2017-09-20 22:14:36 -0500

    initial commit of dbst R package v0.2.0

---
+++ R/dbst.R
@@ -0,0 +1,8 @@
+#' dbst: A package collecting functions and data sets used for DBST 667
+#'
+#' The dbst package contains every data set used throughout the course:
+#'   credit - The CreditApproval dataset.
+#'
+#' @docType package
+#' @name dbst
+NULL
