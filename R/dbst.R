75e929e4b512dceb61fd07eab592794e807b82e0
jmattingly <john.mattingly19@gmail.com>
2019-05-11 14:36:11 -0500

    baselining dbst package
    Executing a baseline of the dbst package due to errors encountered across package versions and content

--- R/dbst.R
+++
@@ -1,9 +0,0 @@
-#' dbst: A package collecting functions and data sets used for DBST 667
-#'
-#' The dbst package contains every data set used throughout the course:
-#'   credit - The CreditApproval dataset.
-#'   cars   - The Cars dataset.
-#'
-#' @docType package
-#' @name dbst
-NULL
